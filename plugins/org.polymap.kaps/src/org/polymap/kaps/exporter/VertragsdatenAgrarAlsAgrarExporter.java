40442d03fa47971525c8908fd6edee7fa465c729
Joerg Reichert <de.abg.reichert.joerg@googlemail.com>
2016-01-06 15:38:31 +0100

    342: Auswertung Agrar mehrerer Gemeinden in einem Vertrag
    Task-Url: http://polymap.org/kaps/ticket/342
    
    Aufschlüsselung nach Flurstücken in Excel-Export

--- plugins/org.polymap.kaps/src/org/polymap/kaps/exporter/VertragsdatenAgrarAlsAgrarExporter.java
+++ plugins/org.polymap.kaps/src/org/polymap/kaps/exporter/VertragsdatenAgrarAlsAgrarExporter.java
@@ -13,7 +13,10 @@
 package org.polymap.kaps.exporter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.polymap.kaps.model.data.FlurstueckComposite;
 import org.polymap.kaps.model.data.GemarkungComposite;
@@ -29,48 +32,87 @@ import org.polymap.kaps.ui.form.EingangsNummerFormatter;
  * @author <a href="http://www.polymap.de">Steffen Stundzig</a>
  */
 public class VertragsdatenAgrarAlsAgrarExporter
-        extends AbstractSingleRowExcelExporter<VertragsdatenAgrarComposite> {
+        extends AbstractExcelExporter<VertragsdatenAgrarComposite> {
 
     public VertragsdatenAgrarAlsAgrarExporter() {
         super( VertragsdatenAgrarComposite.class, VertragsdatenAgrarComposite.NAME, "statistik_agrar", "Verträge" );
     }
 
+    
+    @Override
+    protected List<List<Value>> createMultiRowValues( VertragsdatenAgrarComposite vdc, List<String> errors ) {
+        List<List<Value>> result = new ArrayList<List<Value>>();
 
-    protected List<Value> createValues( VertragsdatenAgrarComposite vdb, List<String> errors ) {
-
-        List<Value> result = new ArrayList<Value>();
-        VertragComposite vertrag = vdb.vertrag().get();
+        VertragComposite vertrag = vdc.vertrag().get();
         if (vertrag == null) {
             errors.add( "Keinen Vertrag gefunden!" );
             return result;
         }
-        FlurstueckComposite flurstueck = null;
+        boolean firstRow = true;
         double verkaufteFlaeche = 0.0d;
-        for (FlurstueckComposite fs : FlurstueckComposite.Mixin.forEntity( vertrag )) {
-            NutzungComposite nutzung = fs.nutzung().get();
-            if (nutzung != null && nutzung.isAgrar().get() != null && nutzung.isAgrar().get() == Boolean.TRUE) {
-                flurstueck = fs;
-                Double flaeche = flurstueck.verkaufteFlaeche().get();
-                if (flaeche != null) {
-                    verkaufteFlaeche += flaeche.doubleValue();
-                }
-            }
-        }
-        if (flurstueck == null) {
+        Iterable<FlurstueckComposite> fses = FlurstueckComposite.Mixin.forEntity( vertrag );
+        if (!fses.iterator().hasNext()) {
             errors.add( error( vertrag, "Kein Flurstück gefunden!" ) );
             return result;
         }
-        GemarkungComposite gemarkung = flurstueck.gemarkung().get();
-        if (gemarkung == null) {
-            errors.add( error( vertrag, "Keine Gemarkung gefunden!" ) );
-            return result;
+        Map<GemarkungComposite, List<FlurstueckComposite>> gemarkungFlurstuecke = new HashMap<GemarkungComposite, List<FlurstueckComposite>>(); 
+        Map<GemeindeComposite, List<GemarkungComposite>> gemeindeGemarkung = new HashMap<GemeindeComposite, List<GemarkungComposite>>(); 
+        for(FlurstueckComposite flurstueck : fses) {
+        	GemarkungComposite gemarkung = flurstueck.gemarkung().get();
+        	if (gemarkung == null) {
+        		errors.add( error( vertrag, String.format("Keine Gemarkung für Flurstück %d gefunden!", flurstueck.name()) ) );
+        		return result;
+        	} else {
+        		if(!gemarkungFlurstuecke.containsKey(gemarkung)) {
+        			gemarkungFlurstuecke.put(gemarkung, new ArrayList<FlurstueckComposite>());
+        		}
+        		gemarkungFlurstuecke.get(gemarkung).add(flurstueck);
+                GemeindeComposite gemeinde = gemarkung.gemeinde().get();
+                if (gemeinde == null) {
+                    errors.add( error( vertrag, "Keine Gemeinde gefunden!" ) );
+                    return result;
+                } else {
+                	if(!gemeindeGemarkung.containsKey(gemeinde)) {
+                		gemeindeGemarkung.put(gemeinde, new ArrayList<GemarkungComposite>());
+                	}
+            		List<GemarkungComposite> gemarkungen = gemeindeGemarkung.get(gemeinde);
+            		if(!gemarkungen.contains(gemarkung)) {
+            			gemarkungen.add(gemarkung);
+            		}
+                }
+        	}
         }
-        GemeindeComposite gemeinde = gemarkung.gemeinde().get();
-        if (gemeinde == null) {
-            errors.add( error( vertrag, "Keine Gemeinde gefunden!" ) );
-            return result;
+        for(Entry<GemeindeComposite, List<GemarkungComposite>> gemeindeGemarkungEntry : gemeindeGemarkung.entrySet()) {
+        	for(GemarkungComposite gemarkung : gemeindeGemarkungEntry.getValue()) {
+            	for(FlurstueckComposite fs : gemarkungFlurstuecke.get(gemarkung)) {
+            		List<Value> createValues = createValues( vertrag, vdc, fs, gemarkung, gemeindeGemarkungEntry.getKey(), verkaufteFlaeche, errors, firstRow );
+            		if (!createValues.isEmpty()) {
+            			result.add( createValues );
+            			firstRow = false;
+            		}
+            	}
+        	}
         }
-        RichtwertzoneComposite zone = flurstueck.richtwertZone().get();
+        List<Value> summary = new ArrayList<Value>();
+        summary.add(new Value( "Vertragsfläche", verkaufteFlaeche, 0 ));
+		result.add( summary );
+        return result;
+    }
+    
+
+    protected List<Value> createValues( VertragComposite vertrag, VertragsdatenAgrarComposite vdb, FlurstueckComposite fs, GemarkungComposite gemarkung, GemeindeComposite gemeinde, 
+    		double verkaufteFlaeche, List<String> errors, boolean firstRow ) {
+        List<Value> result = new ArrayList<Value>();
+        List<FlurstueckComposite> flurstuecke = new ArrayList<FlurstueckComposite>();
+        NutzungComposite nutzung = fs.nutzung().get();
+        if (nutzung != null && nutzung.isAgrar().get() != null && nutzung.isAgrar().get() == Boolean.TRUE) {
+        	flurstuecke.add(fs);
+            Double flaeche = fs.verkaufteFlaeche().get();
+            if (flaeche != null) {
+                verkaufteFlaeche += flaeche.doubleValue();
+            }
+        }
+        RichtwertzoneComposite zone = fs.richtwertZone().get();
         if (zone == null) {
             errors.add( error( vertrag, "Keine Richtwertzone gefunden!" ) );
             return result;
@@ -84,16 +126,18 @@ public class VertragsdatenAgrarAlsAgrarExporter
 
         result.add( new Value( "Eingangsnummer", EingangsNummerFormatter.format( vertrag.eingangsNr().get() ) ) );
         result.add( new Value( "Vertragsdatum", vertrag.vertragsDatum().get() ) );
+        result.add( new Value( "RWZ", zone.name().get() ) );
         result.add( new Value( "Gemeinde", gemeinde.name().get() ) );
         result.add( new Value( "Gemarkung", gemarkung.name().get() ) );
-        result.add( new Value( "Flurstücksnummer", flurstueck.hauptNummer().get() ) );
-        result.add( new Value( "Unternummer", flurstueck.unterNummer().get() ) );
-        result.add( new Value( "Vollpreis", preis, 2 ) );
-        result.add( new Value( "Vertragsfläche gesamt", verkaufteFlaeche, 0 ) );
+        result.add( new Value( "Flurstücksnummer", fs.hauptNummer().get() ) );
+        result.add( new Value( "Unternummer", fs.unterNummer().get() ) );
+        result.add( new Value( "Preis", preis, 2 ) );
+        result.add( new Value( "Vertragsfläche", fs.flaeche().get(), 0 ) );
         result.add( new Value( "Bebaut", vdb.istBebaut().get() ) );
 
         result.add( new Value( "Bodennutzung 1", vdb.bodennutzung1().get() != null ? vdb.bodennutzung1().get().name()
                 .get() : "" ) );
+        
         result.add( new Value( "Fläche 1", vdb.ackerzahl1().get() ) );
         result.add( new Value( "Flächeanteil 1", vdb.flaechenAnteil1().get(), 2 ) );
         result.add( new Value( "Abgleich 1", vdb.abgleichAufKaufpreis1().get(), 2 ) );
